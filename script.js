9c0eb5cb29ed1238501fec9fbb5fecd623ed5e19
getBraga <37493037+getBraga@users.noreply.github.com>
2020-01-24 18:51:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const numeros = [4,11,22,52,34]; /* criando uma array bagunçada*/
-const numeros2 = [4,11,22,52,34,10,,30,20,40,100];
-const numeros3 = [];
-function compararNumeros(a,b) {/* Função para  colocar em ordem*/
-  return  a - b;
+function todosOsNumeros() {
+  
+  const numeros = [4,11,22,52,34]; /* criando uma array bagunçada*/
+  const numeros2 = [4,11,12,22,52,34,10,,30,20,40,100];
+
+  const numeros3 = numeros2.filter( a => !numeros.includes( a ) );
+  
+  function compararNumeros(a,b) {/* Função para  acolocar em ordem*/
+    return  a - b;
+  }
+  numeros3.sort(compararNumeros);
+  console.log(numeros3);
 }
 
 
-numeros2.forEach((elemento) => {
-  if(numeros.indexOf(elemento) === -1)
-  numeros3.push(elemento)
-}) 
-
-numeros3.sort(compararNumeros)
-console.log(numeros3);
-
-
-
+todosOsNumeros();
 
 
 
